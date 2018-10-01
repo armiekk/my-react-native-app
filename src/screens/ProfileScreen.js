7485c1fb96917b0972747fbcdd437a0170e2872a
HITEVENT-IMAC <piyacha@hitevent.com>
2018-10-01 17:38:15 +0700

    add profile page detail

--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -1,9 +1,12 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet, Image, ScrollView } from 'react-native';
+import ElevatedView from 'react-native-elevated-view';
 
+// component
 import AppHeader from '../components/AppHeader';
+import OptionItem from '../components/OptionItem';
 
-export default class ProfileScreen extends React.Component {
+class ProfileScreen extends React.Component {
 
     static navigationOptions = {
         header: props => <AppHeader {...props} title="ข้อมูลส่วนตัว" />,
@@ -11,9 +14,58 @@ export default class ProfileScreen extends React.Component {
 
     render() {
         return (
-            <View>
-                <Text>This is Profile Tab!</Text>
-            </View>
+            <ScrollView style={styles.pageContainer}>
+                <View style={{ flex: 1, justifyContent: 'space-between' }}>
+                    <View style={{ alignItems: 'center', flex: 1 }}>
+                        <ElevatedView
+                            style={styles.userProfileBadgeContainer}
+                            elevation={3}
+                        >
+                            <Image style={styles.userProfileBadge} source={require('../assets/images/person-dummy.jpg')} />
+                        </ElevatedView>
+                        <Text style={styles.userTitleName}>นายกอไก่ ชอบเที่ยวจัง</Text>
+                        <View style={styles.userDetail}>
+                            <Text style={{ fontSize: 20 }}>korkai@hitevent.com</Text>
+                        </View>
+                    </View>
+                    <View style={{ marginTop: 20, borderBottomColor: '#dddddd', borderBottomWidth: 1, borderStyle: 'solid' }}>
+                        <OptionItem title="แก้ไขข้อมูล" style={{ padding: 15 }} iconName="cog" />
+                        <OptionItem title="ออกจากระบบ" style={{ padding: 15 }} iconName="sign-out" />
+                    </View>
+                </View>
+            </ScrollView>
         )
     }
 }
+
+const styles = StyleSheet.create({
+    pageContainer: {
+        backgroundColor: '#fff',
+        flex: 1,
+    },
+    userProfileBadge: {
+        resizeMode: 'contain',
+        height: 200,
+        width: 200,
+    },
+    userProfileBadgeContainer: {
+        marginTop: 20,
+        height: 200,
+        width: 200,
+        borderRadius: 125,
+        overflow: 'hidden'
+    },
+    userTitleName: {
+        fontSize: 30,
+        marginTop: 20,
+        fontWeight: 'bold'
+    },
+    userDetail: {
+        marginTop: 20,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        flex: 1
+    },
+});
+
+export default ProfileScreen;
