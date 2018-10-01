fad480f9bfb4a150d09a0e4d48a047058951e053
HITEVENT-IMAC <piyacha@hitevent.com>
2018-10-01 17:38:52 +0700

    add infinite scroll feature

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,12 +1,28 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Dimensions } from 'react-native';
+import ElevatedView from 'react-native-elevated-view';
 
 // component
 import PostActionButton from './PostActionButton';
 import UserBadge from './UserBadge';
 import FontAwesome from './FontAwesomeIcon';
+import Image from './Image';
 
 class Post extends React.Component {
+
+  // state = {
+  //   image: {
+  //     width: null,
+  //     height: null
+  //   }
+  // }
+
+  // componentDidMount() {
+  //   const { postData } = this.props;
+  //   if (!!postData && !!postData.store_img) {
+  //     this._setImageSize(postData.store_img.imageUrl);
+  //   }
+  // }
 
   _renderPostAction = () => {
     const { postData } = this.props;
@@ -57,28 +73,49 @@ class Post extends React.Component {
     )
   }
 
+  _renderImage = (store_img) => {
+    return (
+      <View style={styles.postImageContainer}>
+        <Image resizeMode="cover" resizeMethod="scale" source={{ uri: store_img.imageUrl }}></Image>
+      </View>
+    )
+  }
+
+  // _setImageSize = (url) => {
+  //   if (!!url) {
+  //     Image.getSize(url, (width, height) => {
+  //       const screenWidth = Dimensions.get('window').width;
+  //       const calculatedHeight = (screenWidth * height) / width;
+  //       this.setState({ image: { width: screenWidth, height: calculatedHeight } })
+  //     });
+  //   }
+  // }
+
   render() {
     const { postData } = this.props;
     return !!postData
       ? (
-        <View style={styles.postContainer}>
-          <View style={{ flexDirection: 'row', marginBottom: 5 }}>
-            <UserBadge style={{ backgroundColor: 'lightgrey' }} />
-            <View style={{ marginLeft: 8 }}>
-              <Text style={{ fontWeight: 'bold', color: '#000', fontSize: 18 }}>{postData.userName || 'GJ JTL'}</Text>
-              <Text style={{ color: 'grey', fontSize: 14 }}>{postData.postTime || '1 min.'} {postData.location || 'TDEX DIVING EXPO'}</Text>
+        <ElevatedView style={styles.postContainer} elevation={2}>
+          <View style={styles.postHeader}>
+            <View style={{ flexDirection: 'row', marginBottom: 5 }}>
+              <UserBadge style={{ backgroundColor: 'lightgrey' }} />
+              <View style={{ marginLeft: 8 }}>
+                <Text style={{ fontWeight: 'bold', color: '#000', fontSize: 18 }}>{postData.userName || 'GJ JTL'}</Text>
+                <Text style={{ color: 'grey', fontSize: 14 }}>{postData.postTime || '1 min.'} {postData.location || 'TDEX DIVING EXPO'}</Text>
+              </View>
+            </View>
+            <View>
+              <Text>{postData.caption || 'this is a tempolary post caption'}</Text>
+            </View>
+            <View style={{ marginTop: 5, flexDirection: 'row' }}>
+              {this._renderLikeCount()}
             </View>
           </View>
-          <View>
-            <Text>{postData.caption || 'this is a tempolary post caption'}</Text>
-          </View>
-          <View style={{ marginTop: 5, flexDirection: 'row' }}>
-            {this._renderLikeCount()}
-          </View>
+          {postData.store_img && this._renderImage(postData.store_img)}
           {/* <View style={{ flexDirection: 'row', justifyContent: 'space-around', marginTop: 5 }}>
             {this._renderPostAction()}
           </View> */}
-        </View>
+        </ElevatedView>
       ) : <View></View>;
   }
 }
@@ -86,9 +123,16 @@ class Post extends React.Component {
 const styles = StyleSheet.create({
   postContainer: {
     backgroundColor: '#fff',
-    padding: 8,
-    marginBottom: 5
+    marginBottom: 10
   },
+  postImageContainer: {
+    flex: 1,
+    marginTop: 5,
+    flexDirection: 'row',
+  },
+  postHeader: {
+    padding: 8
+  }
 });
 
 export default Post;
--- src/screens/DiscoveryScreen.js
+++ src/screens/DiscoveryScreen.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import { View, Text } from 'react-native';
-import AppHeader from '../components/AppHeader';
+import { View, ScrollView, ListView, RefreshControl } from 'react-native';
+import InfiniteScrollView from 'react-native-infinite-scroll-view';
 
 // component
 import Post from '../components/Post';
-
-// style
-import MainStyles from '../constants/MainStyles';
+import AppHeader from '../components/AppHeader';
 
 class DiscoveryScreen extends React.Component {
 
@@ -14,13 +12,115 @@ class DiscoveryScreen extends React.Component {
         header: props => <AppHeader {...props} title="ค้นพบ" />,
     }
 
+    _fakeData = [
+        { postType: 'exibhition', like: { count: 20, } },
+        { postType: 'deal', like: { count: 36, hasLike: true } },
+        { postType: 'deal', like: { count: 36, hasLike: true }, store_img: { imageUrl: 'https://scontent.fbkk5-3.fna.fbcdn.net/v/t31.0-8/16586956_1323896747680700_2967914501562263837_o.jpg?_nc_cat=110&oh=827cf4f2d288d55e28b102cf8e4e470e&oe=5C17C8D1' } },
+        { postType: 'deal', like: { count: 36, hasLike: true }, store_img: { imageUrl: 'https://scontent.fbkk5-3.fna.fbcdn.net/v/t31.0-8/16586956_1323896747680700_2967914501562263837_o.jpg?_nc_cat=110&oh=827cf4f2d288d55e28b102cf8e4e470e&oe=5C17C8D1' } },
+        { postType: 'deal', like: { count: 36, hasLike: true }, store_img: { imageUrl: 'https://scontent.fbkk5-3.fna.fbcdn.net/v/t31.0-8/16586956_1323896747680700_2967914501562263837_o.jpg?_nc_cat=110&oh=827cf4f2d288d55e28b102cf8e4e470e&oe=5C17C8D1' } },
+        { postType: 'deal', like: { count: 36, hasLike: true }, store_img: { imageUrl: 'https://scontent.fbkk5-3.fna.fbcdn.net/v/t31.0-8/16586956_1323896747680700_2967914501562263837_o.jpg?_nc_cat=110&oh=827cf4f2d288d55e28b102cf8e4e470e&oe=5C17C8D1' } },
+        { postType: 'deal', like: { count: 36, hasLike: true }, store_img: { imageUrl: 'https://scontent.fbkk5-3.fna.fbcdn.net/v/t31.0-8/16586956_1323896747680700_2967914501562263837_o.jpg?_nc_cat=110&oh=827cf4f2d288d55e28b102cf8e4e470e&oe=5C17C8D1' } },
+        { postType: 'deal', like: { count: 36, hasLike: true }, store_img: { imageUrl: 'https://scontent.fbkk5-3.fna.fbcdn.net/v/t31.0-8/16586956_1323896747680700_2967914501562263837_o.jpg?_nc_cat=110&oh=827cf4f2d288d55e28b102cf8e4e470e&oe=5C17C8D1' } },
+        { postType: 'deal', like: { count: 36, hasLike: true }, store_img: { imageUrl: 'https://scontent.fbkk5-3.fna.fbcdn.net/v/t31.0-8/16586956_1323896747680700_2967914501562263837_o.jpg?_nc_cat=110&oh=827cf4f2d288d55e28b102cf8e4e470e&oe=5C17C8D1' } },
+        { postType: 'deal', like: { count: 36, hasLike: true }, store_img: { imageUrl: 'https://scontent.fbkk5-3.fna.fbcdn.net/v/t31.0-8/16586956_1323896747680700_2967914501562263837_o.jpg?_nc_cat=110&oh=827cf4f2d288d55e28b102cf8e4e470e&oe=5C17C8D1' } },
+        { postType: 'deal', like: { count: 36, hasLike: true }, store_img: { imageUrl: 'https://scontent.fbkk5-3.fna.fbcdn.net/v/t31.0-8/16586956_1323896747680700_2967914501562263837_o.jpg?_nc_cat=110&oh=827cf4f2d288d55e28b102cf8e4e470e&oe=5C17C8D1' } },
+        { postType: 'deal', like: { count: 36, hasLike: true }, store_img: { imageUrl: 'https://scontent.fbkk5-3.fna.fbcdn.net/v/t31.0-8/16586956_1323896747680700_2967914501562263837_o.jpg?_nc_cat=110&oh=827cf4f2d288d55e28b102cf8e4e470e&oe=5C17C8D1' } },
+        { postType: 'deal', like: { count: 36, hasLike: true }, store_img: { imageUrl: 'https://scontent.fbkk5-3.fna.fbcdn.net/v/t31.0-8/16586956_1323896747680700_2967914501562263837_o.jpg?_nc_cat=110&oh=827cf4f2d288d55e28b102cf8e4e470e&oe=5C17C8D1' } },
+    ]
+
+    constructor(props) {
+        super(props);
+        const postDs = new ListView.DataSource({ rowHasChanged: this._rowHasChanged });
+        this.state = {
+            offset: 0,
+            limit: 5,
+            count: 0,
+            isFetching: false,
+            dataList: [],
+            postDs
+        };
+    }
+
+    async componentDidMount() {
+        await this._updateDataListAsync()();
+    }
+
+    initialStateData = () => {
+        const postDs = new ListView.DataSource({ rowHasChanged: this._rowHasChanged });
+        this.setState({
+            offset: 0,
+            limit: 5,
+            count: 0,
+            isFetching: false,
+            dataList: [],
+            postDs
+        });
+    }
+
+    _updateDataListAsync = ({ offset, limit, isRefresh } = { offset: this.state.offset, limit: this.state.limit, isRefresh: false }) => async () => {
+        if (isRefresh) {
+            this.initialStateData();
+        }
+        this.setState({ isFetching: true });
+        const response = await this._fetchDataAsync(offset, limit);
+        this._setDataList(response);
+    }
+
+    _setDataList = (response) => {
+        const { data: { offset, limit, count, results } } = response;
+        const newDataList = this.state.dataList.concat(results);
+        const newDs = this.state.postDs.cloneWithRows(newDataList);
+        this.setState({
+            dataList: newDataList,
+            postDs: newDs,
+            isFetching: false,
+            offset,
+            limit,
+            count
+        });
+    }
+
+    _fetchDataAsync = (offset, limit) => {
+        return new Promise((resolve, reject) => {
+            const results = this._fakeData.slice(offset, offset + limit);
+            const response = {
+                data: {
+                    offset: offset + results.length,
+                    count: this._fakeData.length,
+                    results,
+                    limit
+                }
+            };
+            setTimeout(() => resolve(response), 1000);
+        });
+    }
+
+    _rowHasChanged = (r1, r2) => {
+        // You might want to use a different comparison mechanism for performance.
+        return JSON.stringify(r1) !== JSON.stringify(r2);
+    }
+
+    _renderRefreshControl() {
+        return (
+            <RefreshControl
+                refreshing={this.state.isFetching}
+                onRefresh={this._updateDataListAsync({ offset: 0, limit: 5, isRefresh: true })}
+            />
+        );
+    }
+
     render() {
         return (
-            <View>
-                <Post postData={{ postType: 'exibhition', like: { count: 20, } }}></Post>
-                <Post postData={{ postType: 'deal', like: { count: 36, hasLike: true } }}></Post>
-                <Post></Post>
-            </View>
+            <ListView
+                renderScrollComponent={props => <InfiniteScrollView {...props} />}
+                dataSource={this.state.postDs}
+                renderRow={rowData => <Post postData={rowData} />}
+                refreshControl={this._renderRefreshControl()}
+                canLoadMore={this.state.offset !== this.state.count}
+                onLoadMoreAsync={this._updateDataListAsync()}
+                distanceToLoadMore={5000}
+                scrollEventThrottle={1000}
+            />
         )
     }
 }
