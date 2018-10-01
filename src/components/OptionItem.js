40c9e28ea92165bfaf4f12e0a9cae19a9e97f79f
HITEVENT-IMAC <piyacha@hitevent.com>
2018-10-01 17:37:56 +0700

    separate post editor option to option item

---
+++ src/components/OptionItem.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { TouchableWithoutFeedback, View, Text } from 'react-native';
+
+// component
+import FontAwesomeIcon from '../components/FontAwesomeIcon';
+
+class OptionItem extends React.Component {
+    render() {
+        const { onPress, title, iconName } = this.props;
+        return (
+            <TouchableWithoutFeedback onPress={() => (!!onPress ? onPress() : {})}>
+                <View
+                    style={[
+                        {
+                            flex: 1,
+                            flexDirection: 'row',
+                            alignItems: 'center',
+                            padding: 25,
+                            borderColor: 'solid',
+                            borderTopWidth: 1,
+                            borderColor: '#dddddd'
+                        },
+                        this.props.style || {}
+                    ]}
+                >
+                    <View
+                        style={{
+                            flexDirection: 'row',
+                            width: 50
+                        }}
+                    >
+                        <FontAwesomeIcon name={iconName} color="#000" />
+                    </View>
+                    <Text style={{ flex: 1 }}>{title}</Text>
+                </View>
+            </TouchableWithoutFeedback>
+        );
+    }
+}
+
+export default OptionItem;
--- src/components/PostEditorOption.js
+++ src/components/PostEditorOption.js
@@ -1,37 +1,12 @@
 import React from 'react';
-import { TouchableWithoutFeedback, View, Text } from 'react-native';
+
 
 // component
-import FontAwesomeIcon from '../components/FontAwesomeIcon';
+import OptionItem from '../components/OptionItem';
 
 class PostEditorOption extends React.Component {
   render() {
-    const { onPress, title, iconName } = this.props;
-    return (
-      <TouchableWithoutFeedback onPress={() => (!!onPress ? onPress() : {})}>
-        <View
-          style={{
-            flex: 1,
-            flexDirection: 'row',
-            alignItems: 'center',
-            padding: 25,
-            borderColor: 'solid',
-            borderTopWidth: 1,
-            borderColor: '#dddddd'
-          }}
-        >
-          <View
-            style={{
-              flexDirection: 'row',
-              width: 50
-            }}
-          >
-            <FontAwesomeIcon name={iconName} color="#000" />
-          </View>
-          <Text style={{ flex: 1 }}>{title}</Text>
-        </View>
-      </TouchableWithoutFeedback>
-    );
+    return <OptionItem {...this.props} />
   }
 }
 
